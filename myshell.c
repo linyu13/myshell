17e98c9f9d2905fa5beb9469dfc3af1c3dc36c0f
linyu13 <3229515040@qq.com>
2024-03-24 13:58:26 +0800

    shell

--- myshell.c
+++ myshell.c
@@ -24,15 +24,17 @@
 
 #define LENGTH 2048
 
-char last_dir[LENGTH] = "";
-char* generate_prompt();
-
+char last_dir[LENGTH] = "";                     // 存储上一个目录
+char* generate_prompt();                        // 生成提示符
+void do_pipe(int left, int right, char** args); // 处理管道命令
+void my_execute(char** args, int cnt);          // 执行命令
 void signal_handler(int signum)
 {
     printf("\n");
     fflush(stdout);
 }
 
+// 生成提示符
 char* generate_prompt()
 {
     // 获取当前时间
@@ -105,7 +107,7 @@ int main()
 {
     signal(SIGINT, signal_handler);
     signal(SIGQUIT, signal_handler);
-    // signal(SIGTSTP, signal_handler);
+    signal(SIGTSTP, signal_handler);
     while (1) {
         if (feof(stdin)) {
             printf("\n");
@@ -128,9 +130,6 @@ int main()
         if (input == NULL || strcmp(input, "") == 0) {
             continue;
         }
-
-        // 将用户输入添加到历史记录
-        // add_history(input);
 
         // 分解用户输入的命令和参数
         char* token;
@@ -164,92 +163,133 @@ int main()
             free(input);
             continue;
         }
-
-        // 检查重定向符号并进行重定向
-        int redirect_out = 0; // 标记是否有输出重定向
-        int redirect_out_app = 0;
-        int redirect_out_over = 0;
-        int redirect_in = 0; // 标记是否有输入重定向
-        char* output_file = NULL;
-        char* input_file = NULL;
-
-        for (int i = 0; i < argc; i++) {
-            if (strcmp(args[i], ">") == 0) {
-                if (i + 1 < argc) {
-                    redirect_out = 1;
-                    redirect_out_over = 1;
-                    output_file = args[i + 1];
-                    args[i] = NULL;
-                    args[i + 1] = NULL;
-                    break;
-                }
-            }
-            else if (strcmp(args[i], ">>") == 0) {
-                if (i + 1 < argc) {
-                    redirect_out = 1; // 追加重定向
-                    redirect_out_app = 1;
-                    output_file = args[i + 1];
-                    args[i] = NULL;
-                    args[i + 1] = NULL;
-                    break;
-                }
-            }
-            else if (strcmp(args[i], "<") == 0) {
-                if (i + 1 < argc) {
-                    redirect_in = 1;
-                    input_file = args[i + 1];
-                    args[i] = NULL;
-                    args[i + 1] = NULL;
-                    break;
-                }
-            }
-        }
-
-        // 创建子进程执行命令
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("fork");
-            free(input);
-            exit(EXIT_FAILURE);
-        }
-        else if (pid == 0) {
-            // 子进程执行命令
-            if (redirect_out) {
-                int fd;
-                if (redirect_out_over) { // 覆盖重定向
-                    fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                }
-                else if (redirect_out_app) { // 追加重定向
-                    fd = open(output_file, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                }
-                if (fd < 0) {
-                    perror("open");
-                    exit(EXIT_FAILURE);
-                }
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-            }
-            if (redirect_in) {
-                int fd = open(input_file, O_RDONLY);
-                if (fd < 0) {
-                    perror("open");
-                    exit(EXIT_FAILURE);
-                }
-                dup2(fd, STDIN_FILENO);
-                close(fd);
-            }
-            execvp(args[0], args);
-            perror("execvp");
-            free(input);
-            exit(EXIT_FAILURE);
-        }
-        else {
-            // 等待子进程执行完成
-            wait(NULL);
-        }
-
-        free(input);
+        my_execute(args, argc);
     }
 
     return 0;
+}
+
+// 执行命令
+void my_execute(char** args, int argc)
+{
+    int background = 0;
+    pid_t pid;
+    pid = fork();
+    if (pid == -1) {
+        printf("无法创建子进程");
+    }
+
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(args[i], "&") == 0) {
+            background = 1;
+            args[i] = NULL;
+            break;
+        }
+    }
+
+    if (pid == 0) {
+        do_pipe(0, argc, args);
+    }
+    else if (pid > 0) {
+        if (background == 0) {
+            waitpid(pid, NULL, 0);
+        }
+    }
+}
+
+// 处理重定向
+void do_others(int left, int right, char** args)
+{
+    int fd;
+
+    for (int i = left; i < right; i++) {
+        if (args[i] == NULL) {
+            continue;
+        }
+
+        if (strcmp(args[i], ">") == 0) { // args[i + 1]为重定向目标
+            fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // 覆盖
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            args[i] = NULL;
+            i++;
+        }
+        if (strcmp(args[i], ">>") == 0) { // args[i + 1]为重定向目标
+            fd = open(args[i + 1], O_WRONLY | O_CREAT | O_APPEND, 0644); // 追加
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            args[i] = NULL;
+            i++;
+        }
+        if (strcmp(args[i], "<") == 0) { // args[i + 1]为被重定向输入的目标
+            fd = open(args[i + 1], O_RDONLY);
+            dup2(
+                fd,
+                STDIN_FILENO); // 通过 dup2() 函数将文件描述符 fd 复制到标准输入 STDIN_FILENO 上
+            close(fd);
+            args[i] = NULL;
+            i++;
+        }
+    }
+
+    char* cmd[LENGTH];
+    for (int i = left; i < right; i++) {
+        cmd[i] = args[i];
+    }
+    cmd[right] = NULL;
+    execvp(cmd[left], cmd + left);
+
+    printf("无效命令\n");
+    exit(EXIT_FAILURE);
+}
+
+// 处理管道
+void do_pipe(int left, int right, char** args)
+{
+    int flag_pipe = -1;
+
+    for (int i = left; i < right; i++) {
+        if (args[i] == NULL) {
+            continue;
+        }
+        if (strcmp(args[i], "|") == 0) {
+            flag_pipe = i;
+            break; // 当读取到管道标志符时，跳出循环处理管道
+        }
+    }
+
+    if (flag_pipe == -1) { // 无管道时进入重定向处理函数
+        do_others(left, right, args);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (flag_pipe + 1 == right) { // 管道在最后一个时的情况
+        printf("|后面缺少参数\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int fd[2];
+    if (pipe(fd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+    pid_t pid = fork();
+    if (pid == -1) {
+        printf("无法创建子进程");
+    }
+
+    if (pid == 0) {   // 子进程中
+        close(fd[0]); // 关闭管道的读端
+        dup2(
+            fd[1],
+            STDOUT_FILENO); // 通过 dup2() 函数将管道的写端 fd[1] 复制到标准输出 STDOUT_FILENO 上
+        do_others(left, flag_pipe, args);
+    }
+    else if (pid > 0) { // 父进程中
+        close(fd[1]);   // 关闭管道的写端
+        dup2(
+            fd[0],
+            STDIN_FILENO); // 通过 dup2() 函数将管道的读端 fd[0] 复制到标准输入 STDIN_FILENO 上
+        do_pipe(flag_pipe + 1, right, args);
+    }
 }
